461ec8889bbf6ecff491e5d0785b668f61d50b6f
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-05-16 17:04:29 +0100

    second chapter on the way!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,37 @@
 $(document).ready(function () {
 
+    $(".introduction").click(showIntroductionChapter);
+
+    function showIntroductionChapter() {
+        $(this).css("opacity", "1");
+        $(".hermetic_philosophy").css("opacity", "0.4");
+        $(".hermetic_philosophy_chapter").fadeOut("fast", function () {
+            $(".introduction_chapter").fadeIn("slow", function () {
+                $(".chapter_underline").animate({
+                    width: "200px",
+                }, 500)
+            });
+        });
+        return false;
+    }
+
+
+    $(".hermetic_philosophy").click(showHermeticPhilosophyChapter);
+
+    function showHermeticPhilosophyChapter() {
+        $(this).css("opacity", "1");
+        $(".introduction_chapter").fadeOut("fast", function () {
+            $(".hermetic_philosophy_chapter").fadeIn("slow", function () {
+                $(".author_small, .hp_quote, .hp_quote2").animate({
+                    opacity: "1",
+                }, 1000)
+                $(".chapter_underline").animate({
+                    width: "400px",
+                }, 500)
+            });
+        });
+        return false;
+    }
+
 
 });
