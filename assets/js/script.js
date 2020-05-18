c11e71e876b242c3d75abe38c7568fa8c02667d6
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-05-18 19:12:52 +0100

    complete re-do

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,52 @@
 $(document).ready(function () {
 
-    $(".introduction").click(showIntroductionChapter);
+    var synoWord = {
+        introduction_chapter: "Introduction",
+        hermetic_philosophy_chapter: "The Hermetic Philosophy",
+        seven_principles_chapter: "The Seven Hermetic Principles",
+        mental_transmutation_chapter: "Mental Transmutation",
+        the_all_chapter: "The All",
+        mental_universe_chapter: "The Mental Universe",
+        divine_paradox_chapter: "The Divine Paradox",
+        all_in_all_chapter: "'The All' in All",
+        correspondence_chapter: "The Planes of Correspondence",
+        vibration_chapter: "Vibration",
+        polarity_chapter: "Polarity",
+        rhythm_chapter: "Rhythm",
+        causation_chapter: "Causation",
+        gender_chapter: "Gender",
+        mental_gender_chapter: "Mental Gender",
+        hermetic_axioms_chapter: "Hermetic Axioms"
+    };
 
-    function showIntroductionChapter() {
-        $(this).css("opacity", "1");
-        $(".hermetic_philosophy, .seven_hermetic_principles, .mental_transmutation").css("opacity", "0.4");
-        $(".seven_principles_chapter, .mental_transmutation_chapter").hide();
+    var maintext = {
+        introduction_chapter: "- i -",
+        hermetic_philosophy_chapter: "I",
+        seven_principles_chapter: "II",
+        mental_transmutation_chapter: "III",
+        the_all_chapter: "IV",
+        mental_universe_chapter: "V",
+        divine_paradox_chapter: "VI",
+        all_in_all_chapter: "VII",
+        correspondence_chapter: "VIII",
+        vibration_chapter: "IX",
+        polarity_chapter: "X",
+        rhythm_chapter: "XI",
+        causation_chapter: "XII",
+        gender_chapter: "XIII",
+        mental_gender_chapter: "XIV",
+        hermetic_axioms_chapter: "XV",
+    };
 
-        $(".hermetic_philosophy_chapter").fadeOut("fast", function () {
-            $(".introduction_chapter").fadeIn("slow", function () {
-                $(".chapter_underline").animate({
-                    width: "200px",
-                }, 500)
-            });
-        });
-        return false;
-    }
+    $("#introduction_chapter, #hermetic_philosophy_chapter, #seven_principles_chapter, #mental_transmutation_chapter, #the_all_chapter, #mental_universe_chapter, #divine_paradox_chapter, #all_in_all_chapter, #correspondence_chapter, #vibration_chapter, #polarity_chapter, #rhythm_chapter, #causation_chapter, #gender_chapter, #mental_gender_chapter, #hermetic_axioms_chapter").hover(
+        function () {
+            $(this).text(synoWord[this.id]);
 
+        },
+        function () {
+            $(this).text(maintext[this.id]);
 
-    $(".hermetic_philosophy").click(showHermeticPhilosophyChapter);
-
-    function showHermeticPhilosophyChapter() {
-        $(this).css("opacity", "1");
-        $(".introduction, .seven_hermetic_principles").css("opacity", "0.4");
-        $(".seven_principles_chapter, .introduction_chapter, .mental_transmutation_chapter").hide();
-
-        $(".introduction_chapter").fadeOut("fast", function () {
-            $(".hermetic_philosophy_chapter").fadeIn("slow", function () {
-                $(".author_small, .hp_quote, .hp_quote2").animate({
-                    opacity: "0.7",
-                }, 1000);
-                $(".hp_quote3, .sp_quote").animate({
-                    opacity: "0.7",
-                }, 1000)
-                $(".chapter_underline").animate({
-                    width: "400px",
-                }, 500)
-            });
-        });
-        return false;
-    }
-
-
-    $(".seven_hermetic_principles").click(showSevenHermeticChapter);
-
-    function showSevenHermeticChapter() {
-        $(this).css("opacity", "1");
-        $(".introduction, .hermetic_philosophy, .mental_transmutation").css("opacity", "0.4");
-        $(".introduction_chapter, .mental_transmutation_chapter").hide();
-
-        $(".hermetic_philosophy_chapter").fadeOut("fast", function () {
-            $(".seven_principles_chapter").fadeIn("slow", function () {
-                $(".chapter_underline").animate({
-                    width: "500px",
-                }, 500)
-            });
-        });
-        return false;
-    }
-
-    $(".mental_transmutation").click(showMentalTransChapter);
-
-    function showMentalTransChapter() {
-        $(this).css("opacity", "1");
-        $(".introduction, .hermetic_philosophy, .seven_hermetic_principles").css("opacity", "0.4");
-        $(".introduction_chapter, .seven_principles_chapter").hide();
-
-        $(".hermetic_philosophy_chapter").fadeOut("fast", function () {
-            $(".mental_transmutation_chapter").fadeIn("slow", function () {
-                $(".chapter_underline").animate({
-                    width: "600px",
-                }, 500)
-            });
-        });
-        return false;
-    }
-
-
-
+        }
+    );
 
 });
